e41fc77ed903d25d9143242c0083b95f795e2983
Romain Crestey <rcrestey@gmail.com>
2025-08-05 22:42:36 +0200

    add Stack component

---
+++ src/lib/components/Stack.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { iconByLang, type Lang } from '$lib/icons';
+
+	type Props = {
+		stack?: Lang[];
+	};
+
+	const { stack = [] }: Props = $props();
+
+	// sveltekit is not available as icon
+	const cleanStack = $derived(stack.filter((lang) => Object.keys(iconByLang).includes(lang)));
+</script>
+
+<div class="Stack">
+	{#each cleanStack as icon (icon)}
+		<i class="nf {iconByLang[icon]}"></i>
+	{/each}
+</div>
+
+<style>
+	.Stack {
+		display: flex;
+		gap: 0.5rem;
+	}
+</style>
